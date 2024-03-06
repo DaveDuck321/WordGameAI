0e3b6cd917c56adfc154c6f57e082fef4e280615
Tom Grant <thomas.grant.duck321@gmail.com>
2024-03-06 23:37:03 +0000

    Add unix wordlist

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,4 +1,4 @@
-export const MAX_WORD_LENGTH = 13
+export const MAX_WORD_LENGTH = 6
 export const MAX_CHALLENGES = 6
 export const ALERT_TIME_MS = 2000
 export const ENABLE_SAVE_GAME_PROGRESS = false
